6e8f1913150204bd75e4a2fa75eb1fde37de0b46
patrickluvsoj <pasuzuk@microsoft.com>
2018-12-25 14:13:49 -0800

    login requirement & show posts

--- blog.py
+++ blog.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, \
 flash, redirect, url_for, g
+from functools import wraps
 import sqlite3
 
 DATABASE = 'blog.db'
@@ -12,8 +13,20 @@ app = Flask(__name__)
 
 app.config.from_object(__name__)
 
-def connect():
+def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You need to log in first.")
+            return redirect(url_for('login'))
+    return wrap
+
 
 @app.route('/', methods=['GET','POST'])
 def login():
@@ -30,11 +43,15 @@ def login():
     return render_template('login.html', error=error), status_code
 
 
-    return render_template('login.html')
-
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute("SELECT * FROM posts")
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
 
 @app.route('/logout')
 def logout():
