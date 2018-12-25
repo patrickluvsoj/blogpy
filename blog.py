7748ad96e5a8a311b25bfaf41a125022c6e8cc62
patrickluvsoj <pasuzuk@microsoft.com>
2018-12-25 15:22:03 -0800

    finish blog app

--- blog.py
+++ blog.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, request, session, \
 flash, redirect, url_for, g
-from functools import wraps
+from functools import wraps # used to create python decorators
 import sqlite3
 
+# flask config variables
 DATABASE = 'blog.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 SECRET_KEY = 'qfKWWbF7nQyHczXdugfzvdNy'
 
-
+# instantiate flask app
 app = Flask(__name__)
 
+# passing config varibales to app
 app.config.from_object(__name__)
 
+# method that returns sql connection
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-
+# decorator method to ensure user is logged in
 def login_required(test):
     @wraps(test)
     def wrap(*args, **kwargs):
@@ -27,7 +30,7 @@ def login_required(test):
             return redirect(url_for('login'))
     return wrap
 
-
+# root authentication page. takes you to main if login succcessful.
 @app.route('/', methods=['GET','POST'])
 def login():
     error = None
@@ -42,7 +45,7 @@ def login():
             return redirect(url_for('main'))
     return render_template('login.html', error=error), status_code
 
-
+# main page. displays blog posts by querying sql
 @app.route('/main')
 @login_required
 def main():
@@ -52,14 +55,34 @@ def main():
     g.db.close()
     return render_template('main.html', posts=posts)
 
-
+# logout page/button
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
 
+# posting page/button
+@app.route('/add', methods=["POST"])
+@login_required
+def add():
+    # get user data input from html form
+    title = request.form['title']
+    post = request.form['post']
+    # check if title and post is filled in. if not reload page
+    if not title or not post:
+        flash("Title and post fields are required. Please try again.")
+        return redirect(url_for('main'))
+    # insert title and post into sql
+    else:
+        g.db = connect_db()
+        g.db.execute("INSERT INTO posts (title, post) VALUES(?,?)", 
+        [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash("New entry was successfully posted")
+        return redirect(url_for('main'))
 
+# run app / debug on
 if __name__ == '__main__':
     app.run(debug=True)
-
